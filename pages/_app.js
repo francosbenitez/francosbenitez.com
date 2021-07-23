638dc48aa1517602b0e16daa22c93acd42cce46a
francosbenitez <francosbenitez@gmail.com>
2021-07-23 10:12:55 -0300

    create theme and global style

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,37 @@
-import '../styles/globals.css'
+/*import '../styles/globals.css'*/
+import { createGlobalStyle, ThemeProvider } from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+}
+
+  * {
+    font-family: 'Inter', sans-serif;
+  }
+
+  a {
+    text-decoration: none;
+  }
+`;
+
+const theme = {
+  font: "Inter",
+  primary: "#2079c7",
+  secondary: "#666666",
+}
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  </>
+  )
 }
 
 export default MyApp
