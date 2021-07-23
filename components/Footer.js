4f33ed27a116952e6069d1cfc9d2d5f3ede67899
francosbenitez <francosbenitez@gmail.com>
2021-07-23 11:52:56 -0300

    add footer

---
+++ components/Footer.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer className="site-footer">
+            <p>Built with ☕️ and Next by 
+            <a 
+                href="https://twitter.com/CL_Rothschild"
+                target="_blank"
+                rel="noopener noreferrer"
+                class="link"
+                > Franco Sebastián Benítez
+            </a>. The source code is available on 
+            <a
+                href="https://github.com/francosbenitez/francosbenitez.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                class="link"
+                > GitHub</a
+            >.
+            </p>
+        </footer>
+    );
+};
+
+export default Footer;
