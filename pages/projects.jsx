21f7e166cfd16266a2530ccdeec900677fa69105
francosbenitez <francosbenitez@gmail.com>
2021-08-02 15:49:15 -0300

    feat: projects & blog pages created

---
+++ pages/projects.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Projects = () => {
+    return (
+        <div>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            Hello World 2.0
+        </div>
+    );
+};
+
+export default Projects;
