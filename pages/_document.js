ff9dc6213416a95ac94fd40c1cd5f878bfeaaf24
francosbenitez <francosbenitez@gmail.com>
2021-08-08 19:36:53 -0300

    fix: add ibm plex sans font

--- pages/_document.js
+++ pages/_document.js
@@ -31,7 +31,9 @@ export default class CustomDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans&display=swap" rel="stylesheet" />
+        </Head>
         <body>
           <Main />
           <NextScript />
