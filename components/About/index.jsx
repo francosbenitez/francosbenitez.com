41c1c6bbf1ee52971a544f60dd083c1ad32c08c5
francosbenitez <francosbenitez@gmail.com>
2021-08-07 23:27:17 -0300

    style: move skills styles to styled cards

--- components/About/index.jsx
+++ components/About/index.jsx
@@ -8,14 +8,15 @@ import Python from "../../public/icons/tech/python.svg"
 import CSS from"../../public/icons/tech/css.svg"
 import Styled from "../../public/icons/tech/styled.svg"
 import Git from "../../public/icons/tech/git.svg"*/}
-import styles from './SkillsSection.module.scss';
+//import styles from './SkillsSection.module.scss';
+import { StyledSkillsCard } from "../../styles/Cards"
 
 const About = () => {
     const techFunc = (content) => {
         return (
             content.map((tech, i) => {
                 return (
-                    <div key={i} className={styles.item}>
+                    <div key={i} className="item">
                         <Image
                         src={tech.url}
                         alt={tech.name}
@@ -67,61 +68,63 @@ const About = () => {
                 </p>
                 </div>
             </div>
-            <section className={styles.container}>
+            <StyledSkillsCard>
+            <section className="container">
                 <h1>
                     <span>
                         {content.about.techStack.title[0]}
                     </span>
                     {" "}{content.about.techStack.title[1]}
                 </h1>
-                <div className={styles.cardsGrid}>
-                    <div className={styles.cardWrapper}>
+                <div className="cardsGrid">
+                    <div className="cardWrapper">
                         <h2>{content.about.techStack.front.title}</h2>
-                        <div className={styles.card}>
-                            <div className={styles.row}>
+                        <div className="card">
+                            <div className="row">
                                 {techFunc(content.about.techStack.front.firstRow)}
                             </div>
-                            <div className={styles.row}>
+                            <div className="row">
                                 {techFunc(content.about.techStack.front.secondRow)}
                             </div>
-                            <div className={styles.row}>
+                            <div className="row">
                                 {techFunc(content.about.techStack.front.thirdRow)}
                             </div>
-                            <div className={styles.row}>
+                            <div className="row">
                                 {techFunc(content.about.techStack.front.fourthRow)}
                             </div>
                         </div>
                     </div>
-                    <div className={styles.cardWrapper}>
+                    <div className="cardWrapper">
                         <h2>{content.about.techStack.back.title}</h2>
-                        <div className={styles.card}>
-                            <div className={styles.row}>
+                        <div className="card">
+                            <div className="row">
                                 {techFunc(content.about.techStack.back.firstRow)}
                             </div>
-                            <div className={styles.row}>
+                            <div className="row">
                                 {techFunc(content.about.techStack.back.secondRow)}
                             </div>
-                            <div className={styles.row}>
+                            <div className="row">
                                 {techFunc(content.about.techStack.back.thirdRow)}
                             </div>
                         </div>
                     </div>
-                    <div className={styles.cardWrapper}>
+                    <div className="cardWrapper">
                         <h2>{content.about.techStack.misc.title}</h2>
-                        <div className={styles.card}>
-                            <div className={styles.row}>
+                        <div className="card">
+                            <div className="row">
                                 {techFunc(content.about.techStack.misc.firstRow)}
                             </div>
-                            <div className={styles.row}>
+                            <div className="row">
                                 {techFunc(content.about.techStack.misc.secondRow)}
                             </div>
-                            <div className={styles.row}>
+                            <div className="row">
                                 {techFunc(content.about.techStack.misc.thirdRow)}
                             </div>
                         </div>
                     </div>
                 </div>
             </section>
+            </StyledSkillsCard>
         </StyledAbout>
     );
 };
--- styles/Cards.jsx
+++ styles/Cards.jsx
@@ -107,5 +107,89 @@ export const StyledProjectsCard = styled.div`
     }
 `
 
+export const StyledSkillsCard = styled.div`
+    .container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 100%;
+  padding: 30px 30px;
+  margin-top: 50px;
+  margin-bottom: 50px;
+}
+
+.cardsGrid {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+
+  margin-top: 50px;
+
+  @media (min-width: 850px) {
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+
+    .cardWrapper {
+      margin: 0 16px;
+    }
+  }
+}
+
+.cardWrapper {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h2 {
+    /*color: white;
+    font-family: 'Big Shoulders Stencil Display', cursive;*/
+    font-size: 24px;
+    font-weight: 400;
+    text-align: center;
+  }
+
+  .card {
+    width: calc(min(100%, 300px));
+    height: 400px;
+    margin: 16px 0 30px;
+
+    background-color: black;
+    border-radius: 10px;
+
+    padding: 10px 8px 40px;
+
+    display: flex;
+    flex-direction: column;
+
+    .row {
+      flex: 1;
+      display: flex;
+      align-items: flex-end;
+      justify-content: space-evenly;
+
+      .item {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        transition: 0.2s;
+
+        h3 {
+          color: white;
+          text-align: center;
+          margin-top: 8px;
+        }
+
+        &:hover {
+          transform: scale(1.1);
+        }
+      }
+    }
+  }
+}
+
+`;
 
 
