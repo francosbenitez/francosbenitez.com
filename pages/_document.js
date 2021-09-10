576267416d3b320184cb238700d8dcd2245fb2ca
francosbenitez <francosbenitez@gmail.com>
2021-09-10 14:36:11 -0300

    style(fonts): upload all the IBM Plex Sans fonts

--- pages/_document.js
+++ pages/_document.js
@@ -32,7 +32,7 @@ export default class CustomDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet" />
           <meta name="theme-color" content={`$${props => props.theme.colors.primary}`} />
         </Head>
         <body>
