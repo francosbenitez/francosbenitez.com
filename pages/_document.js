e75b091c1f892db3a3b92e7c35dfd6daa2c22ea3
francosbenitez <francosbenitez@gmail.com>
2021-09-18 09:53:49 -0300

    feat: add preconnect for fonts

--- pages/_document.js
+++ pages/_document.js
@@ -32,6 +32,8 @@ export default class CustomDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap" rel="stylesheet" />
           <meta name="theme-color" content={`$${props => props.theme.colors.primary}`} />
