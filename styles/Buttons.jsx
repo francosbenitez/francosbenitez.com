4728ca6473ce0ba07981245f6a39c5fe94ff9d8f
Franco Sebastián Benítez <62864550+francosbenitez@users.noreply.github.com>
2021-09-25 22:26:27 -0300

    Update Buttons.jsx

--- styles/Buttons.jsx
+++ styles/Buttons.jsx
@@ -11,7 +11,7 @@ export const StyledProjectsButton = styled.div`
         border:1px solid ${props => props.theme.colors.primary};
         display:inline-block;
         cursor:pointer;
-        color: ${props => props.theme.colors.background};
+        color: #F4F4F4;
         font-size:120%;
         font-weight: 700;
         padding:.5rem 1rem;
