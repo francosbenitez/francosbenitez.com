c12e36da37e1ee6a3772206470b19819ddd76962
francobenitez <franco.benitez@hogarthww.com>
2022-07-05 21:35:38 -0300

    feat: add favicon & others to layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,13 +2,46 @@ import { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme, GlobalStyle } from "../styles/Theme";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import Head from "next/head";
 
 const Layout = ({ children }: { children: any }) => {
   return (
     <>
       <ThemeProvider theme={lightTheme}>
         <GlobalStyle />
-        Layout
+        <Head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/favicon_io/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon_io/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon_io/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/favicon_io/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/favicon_io/safari-pinned-tab.svg"
+            color="#919191"
+          />
+          <meta
+            name="msapplication-TileColor"
+            content={`${(props: any) => props.theme.colors.primary}`}
+          />
+          <meta
+            name="theme-color"
+            content={`${(props: any) => props.theme.colors.primary}`}
+          />
+        </Head>
         <Navbar />
         {children}
         <Footer />
