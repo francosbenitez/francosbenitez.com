f8a3165648df20bf7458e88ca1e70703ac507b33
Franco <francosbenitez@gmail.com>
2024-07-03 00:19:16 -0300

    feat: _app improvements

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,39 @@
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
 import "../styles/global.css";
+import es from "lang/es.json";
+import en from "lang/en.json";
+import { IntlProvider } from "react-intl";
+import LocaleProvider from "contexts/LocaleContext/LocaleProvider";
+import LocaleConsumer from "contexts/LocaleContext/LocaleConsumer";
 
-function MyApp({ Component, pageProps }: AppProps) {
+const App = ({ Component, pageProps }: AppProps) => {
+  const messages = {
+    es,
+    en,
+  };
+
   return (
     <>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <LocaleProvider>
+        <LocaleConsumer>
+          {({ currentLocale }) => (
+            <>
+              <IntlProvider
+                messages={messages[currentLocale]}
+                locale={currentLocale}
+                defaultLocale={currentLocale}
+              >
+                <Layout>
+                  <Component {...pageProps} />
+                </Layout>
+              </IntlProvider>
+            </>
+          )}
+        </LocaleConsumer>
+      </LocaleProvider>
     </>
   );
-}
+};
 
-export default MyApp;
+export default App;
