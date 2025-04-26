4f57ffe463341ea6f6563413f68827e2d1f06737
Franco Sebastian Benitez <francosbenitez@gmail.com>
2025-04-25 23:11:41 -0300

    feat: rehype-raw

--- lib/markdown.tsx
+++ lib/markdown.tsx
@@ -3,12 +3,17 @@ import remarkParse from "remark-parse"
 import remarkRehype from "remark-rehype"
 import rehypeStringify from "rehype-stringify"
 import remarkGfm from "remark-gfm"
+// import rehypeRaw from "rehype-raw"
 
 export async function markdownToHtml(markdown: string) {
   const result = await unified()
     .use(remarkParse)
     .use(remarkGfm)
     .use(remarkRehype)
+    // NOTE: The commented code
+    // below is for rendering img, Image, etc., in .mdx files
+    // .use(remarkRehype, { allowDangerousHtml: true })
+    // .use(rehypeRaw)
     .use(rehypeStringify)
     .process(markdown)
 
